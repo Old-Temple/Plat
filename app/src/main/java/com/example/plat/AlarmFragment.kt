ede1a367c3af3b9b2c2f86c924a7b25b61459794
Old-Temple <78444302+Old-Temple@users.noreply.github.com>
2021-04-08 07:46:32 +0900

    [영현] 서치화면 구성
    리스트 손봤음

--- app/src/main/java/com/example/plat/AlarmFragment.kt
+++ app/src/main/java/com/example/plat/AlarmFragment.kt
@@ -1,27 +1,54 @@
 package com.example.plat
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
 
 /**
  * A simple [Fragment] subclass.
  * 알람창 화면
  */
 class AlarmFragment : Fragment() {
+    //임시로 만든 arraylist
+    private val myList = arrayListOf<AlarmContentsListItem>(
+        AlarmContentsListItem("Image1", "User1", "Like"),
+        AlarmContentsListItem("Image2", "User2", "Share"),
+        AlarmContentsListItem("Image3", "User3", "Like"),
+        AlarmContentsListItem("Image4", "User4", "Like"),
+        AlarmContentsListItem("Image5", "User5", "Share"),
+        AlarmContentsListItem("Image6", "User6", "Like"),
+        AlarmContentsListItem("Image7", "User7", "Share"),
+        AlarmContentsListItem("Image8", "User8", "Share"),
+        AlarmContentsListItem("Image9", "User9", "Like"),
+        AlarmContentsListItem("Image0", "User0", "Like"),
+        AlarmContentsListItem("Image1", "User1", "Share"),
+        AlarmContentsListItem("Image2", "User2", "Share"),
+        AlarmContentsListItem("Image3", "User3", "Like"),
+        AlarmContentsListItem("Image4", "User4", "Like"),
+        AlarmContentsListItem("Image5", "User5", "Share"),
+        AlarmContentsListItem("Image6", "User6", "Like")
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_alarm, null)
+        val alarmListView = view.findViewById<ListView>(R.id.alarmListView)
+        val alarmListAdapter = AlarmListAdapter(activity!!, myList)
+
+        alarmListView.adapter = alarmListAdapter
+
+        return view
     }
 
-<<<<<<< Updated upstream
-=======
 }
 
 //이미지의 경우 임시로 String타입 줬음
@@ -60,8 +87,4 @@ class AlarmListAdapter(val context: Context, val alarmContentsListItem: ArrayLis
     override fun getItemId(position: Int): Long {
         return 0
     }
-
-
-
->>>>>>> Stashed changes
 }
--- app/src/main/java/com/example/plat/SearchFragment.kt
+++ app/src/main/java/com/example/plat/SearchFragment.kt
@@ -1,23 +1,92 @@
 package com.example.plat
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.GridView
+import android.widget.TextView
 
 /**
- * A simple [Fragment] subclass.
  * 서치 창 화면
  */
 class SearchFragment : Fragment() {
 
+    private val myList = arrayListOf<SearchContentsListItem>(
+        SearchContentsListItem("Image1","Text1"),
+        SearchContentsListItem("Image2","Text2"),
+        SearchContentsListItem("Image3","Text3"),
+        SearchContentsListItem("Image4","Text4"),
+        SearchContentsListItem("Image5","Text5"),
+        SearchContentsListItem("Image6","Text6"),
+        SearchContentsListItem("Image7","Text7"),
+        SearchContentsListItem("Image8","Text8"),
+        SearchContentsListItem("Image9","Text9"),
+        SearchContentsListItem("Image0","Text0"),
+        SearchContentsListItem("Image9","Text9"),
+        SearchContentsListItem("Image8","Text8"),
+        SearchContentsListItem("Image7","Text7"),
+        SearchContentsListItem("Image6","Text6"),
+        SearchContentsListItem("Image5","Text5"),
+        SearchContentsListItem("Image4","Text4"),
+        SearchContentsListItem("Image3","Text3"),
+        SearchContentsListItem("Image2","Text2"),
+        SearchContentsListItem("Image1","Text1"),
+        SearchContentsListItem("Image2","Text2"),
+        SearchContentsListItem("Image3","Text3"),
+        SearchContentsListItem("Image4","Text4"),
+        SearchContentsListItem("Image5","Text5")
+    )
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_search, null)
+        val searchListView = view.findViewById<GridView>(R.id.searchGridView)
+        val searchListAdapter = SearchListAdapter(activity!!, myList)
+
+        searchListView.adapter = searchListAdapter
+
+        return view
+    }
+
+}
+
+//이미지의 경우 임시로 String타입 줬음
+class SearchContentsListItem(val image: String, val text: String)
+
+//listAdapter
+class SearchListAdapter(val context: Context, val searchContentsListItem: ArrayList<SearchContentsListItem>) : BaseAdapter(){
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        //각 리스트 형태 저장
+        val view: View = LayoutInflater.from(context).inflate(R.layout.list_item_search, null)
+
+        //각 리스트 내용
+        val image = view.findViewById<TextView>(R.id.searchImage)
+        val text = view.findViewById<TextView>(R.id.searchText)
+        val item = searchContentsListItem[position]
+
+        image.text = item.image
+        text.text = item.text
+
+        return view
+    }
+
+    override fun getCount(): Int {
+        return searchContentsListItem.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return searchContentsListItem[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
     }
 
 }
