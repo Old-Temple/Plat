f05e5d625daf6d3a5f676e299c41e4693934c5f1
Old-Temple <78444302+Old-Temple@users.noreply.github.com>
2021-04-07 14:20:26 +0900

    [영현]프로필화면리스트 구현
    각 리스트 상세 구현해둠

--- app/src/main/java/com/example/plat/ProfileFragment.kt
+++ app/src/main/java/com/example/plat/ProfileFragment.kt
@@ -1,17 +1,16 @@
 package com.example.plat
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
+import org.w3c.dom.Text
 
 /**
  * 프로필 화면
@@ -20,12 +19,22 @@ import android.widget.TextView
  */
 class ProfileFragment : Fragment() {
     //임시로 만든 arraylist
-    val myList = arrayListOf<ProfileListItem>(
-        ProfileListItem("리스트1"), ProfileListItem("리스트2"), ProfileListItem("리스트3"),
-        ProfileListItem("리스트4"), ProfileListItem("리스트5"), ProfileListItem("리스트6"),
-        ProfileListItem("리스트7"), ProfileListItem("리스트8"), ProfileListItem("리스트9"),
-        ProfileListItem("리스트10"), ProfileListItem("리스트11"), ProfileListItem("리스트12"),
-        ProfileListItem("리스트13"), ProfileListItem("리스트14"), ProfileListItem("리스트15")
+    val myList = arrayListOf<ProfileContentsListItem>(
+        ProfileContentsListItem("Imgae1", "Title1", "Text1"),
+        ProfileContentsListItem("Imgae2", "Title2", "Text2"),
+        ProfileContentsListItem("Imgae3", "Title3", "Text3"),
+        ProfileContentsListItem("Imgae4", "Title4", "Text4"),
+        ProfileContentsListItem("Imgae5", "Title5", "Text5"),
+        ProfileContentsListItem("Imgae6", "Title6", "Text6"),
+        ProfileContentsListItem("Imgae7", "Title7", "Text7"),
+        ProfileContentsListItem("Imgae8", "Title8", "Text8"),
+        ProfileContentsListItem("Imgae9", "Title9", "Text9"),
+        ProfileContentsListItem("Imgae10", "Title10", "Text10"),
+        ProfileContentsListItem("Imgae11", "Title11", "Text11"),
+        ProfileContentsListItem("Imgae12", "Title12", "Text12"),
+        ProfileContentsListItem("Imgae13", "Title13", "Text13"),
+        ProfileContentsListItem("Imgae14", "Title14", "Text14"),
+        ProfileContentsListItem("Imgae15", "Title15", "Text15")
     )
 
     override fun onCreateView(
@@ -44,11 +53,11 @@ class ProfileFragment : Fragment() {
 
 }
 
-//임시 클래스(이름 바꿀 예정)
-class ProfileListItem(val text: String)
+//이미지의 경우 임시로 String타입 줬음
+class ProfileContentsListItem(val image: String, val title: String, val text: String)
 
 //테스트용 listadapter
-class ProfileListAdapter (val context: Context, val profileListItem: ArrayList<ProfileListItem>) : BaseAdapter() {
+class ProfileListAdapter (val context: Context, val profileContentsListItem: ArrayList<ProfileContentsListItem>) : BaseAdapter() {
     
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View{
         
@@ -56,21 +65,25 @@ class ProfileListAdapter (val context: Context, val profileListItem: ArrayList<P
         val view: View = LayoutInflater.from(context).inflate(R.layout.profile_list_item, null)
         
         //각 리스트의 내용
-        val textItem = view.findViewById<TextView>(R.id.profileContents)
-        val item = profileListItem[position]
-        textItem.text = item.text
+        val image = view.findViewById<TextView>(R.id.contentsImage)
+        val title = view.findViewById<TextView>(R.id.contentsTitle)
+        val text = view.findViewById<TextView>(R.id.contentsText)
+        val item = profileContentsListItem[position]
+        image.text = item.image
+        title.text = item.title
+        text.text = item.text
 
         return view
     }
     //오버라이드 함수
     //리스트 크기 반환함
     override fun getCount(): Int {
-        return profileListItem.size
+        return profileContentsListItem.size
     }
     //오버라이드 함수
     //해당 인덱스의 아이템 반환함
     override fun getItem(position: Int): Any {
-        return profileListItem[position]
+        return profileContentsListItem[position]
     }
     //오버라이드 함수
     //해당 인덱스의 아이디 반환함
