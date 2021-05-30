91c60c034b5b6415c938962e0daa827903157b55
NATAnine <qwertty16@naver.com>
2021-05-30 16:30:31 +0900

    [예솔] 플랏리스트 보이는버전

--- app/src/main/java/com/example/plat/MainFragment.kt
+++ app/src/main/java/com/example/plat/MainFragment.kt
@@ -586,7 +586,7 @@ class MainFragment(val mainActivity: MainActivity) : Fragment() {
         //코루틴 안에서 정보를 받아온 후에 프래그먼트 뷰 시킴
 
             val fragmentTransactionListener: FragmentTransaction = childFragmentManager.beginTransaction()
-            fragmentTransactionListener.replace(R.id.plat_fragment_root, MainChildPlat())
+            fragmentTransactionListener.replace(R.id.plat_scroll_root, MainChildPlat())
             fragmentTransactionListener.commit()
 
     }
@@ -646,7 +646,7 @@ class MainChildPlat() : Fragment(){
 
         val platlistView2 = view.findViewById<FrameLayout>(R.id.PlatFrame)
 
-        platlistView2.addView(view)
+//        platlistView2.addView(view)
 
         return view
     }
